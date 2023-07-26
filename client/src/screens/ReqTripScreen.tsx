9b82fb4b53f62a77520b7cc2501fad192638237d
erfajc97 <erfacrypto@gmail.com>
2023-07-26 13:42:13 -0300

    add map

--- client/src/screens/ReqTripScreen.tsx
+++ client/src/screens/ReqTripScreen.tsx
@@ -1,21 +1,36 @@
-import { View, Text, Image, TouchableOpacity, ScrollView, StyleProp, ViewStyle } from 'react-native'
-import React, { useState } from 'react'
-import star from "../assets/icons/StarFull.png"
-import { AirbnbRating } from 'react-native-ratings';
-import ChatCircleText from "../assets/icons/ChatCircleText.svg"
-import ArrowBackComponent from '../components/ArrowBackComponent';
-import Calendar from '../assets/icons/Calendar.svg'
-import PointedLine from '../assets/icons/PointedLine.svg'
-import CircleBlack from '../assets/icons/CircleBlack.svg'
-import MapPin from '../assets/icons/MapPin.svg'
-import Clock from '../assets/icons/Clock.svg'
-import Car from '../assets/icons/Car.svg'
-import CreditCard from '../assets/icons/CreditCard.svg'
-import Visa from '../assets/icons/Visa.svg'
-import ArrowDropDown from '../assets/icons/ArrowDropDown.svg'
-import ButtonReqTravel from '../components/ButtonReqTravel';
-import { RouteProp } from '@react-navigation/native';
-const StarRating = ({ rating, styles }: { rating?: number, styles: StyleProp<ViewStyle> }) => {
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import React, { useState } from "react";
+import star from "../assets/icons/StarFull.png";
+import { AirbnbRating } from "react-native-ratings";
+import ChatCircleText from "../assets/icons/ChatCircleText.svg";
+import ArrowBackComponent from "../components/ArrowBackComponent";
+import Calendar from "../assets/icons/Calendar.svg";
+import PointedLine from "../assets/icons/PointedLine.svg";
+import CircleBlack from "../assets/icons/CircleBlack.svg";
+import MapPin from "../assets/icons/MapPin.svg";
+import Clock from "../assets/icons/Clock.svg";
+import Car from "../assets/icons/Car.svg";
+import CreditCard from "../assets/icons/CreditCard.svg";
+import Visa from "../assets/icons/Visa.svg";
+import ArrowDropDown from "../assets/icons/ArrowDropDown.svg";
+import ButtonReqTravel from "../components/ButtonReqTravel";
+import { RouteProp } from "@react-navigation/native";
+import MapView from "react-native-maps";
+const StarRating = ({
+  rating,
+  styles,
+}: {
+  rating?: number;
+  styles: StyleProp<ViewStyle>;
+}) => {
   return (
     <AirbnbRating
       count={5}
@@ -26,26 +41,50 @@ const StarRating = ({ rating, styles }: { rating?: number, styles: StyleProp<Vie
       isDisabled={true}
       starContainerStyle={styles}
     />
-  )
-}
+  );
+};
 
 const ReqTripScreen = ({ route }: { route: RouteProp<any> }) => {
-  const { name, picture, location, day } = route.params?.item
-  const [requestSent, setRequestSent] = useState(false)
-  const [requestAccepted] = useState(false)
+  const { name, picture, location, day } = route.params?.item;
+  const [requestSent, setRequestSent] = useState(false);
+  const [requestAccepted] = useState(false);
+  const [origin, setOrigin] = useState({
+    latitude: -34.604442,
+    longitude: -58.430154,
+  });
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} >
-      <ArrowBackComponent edit='absolute top-6 left-5 z-10 rounded-full p-1 bg-white'/>
-      <View className={`h-[130px] max-h-[18%] border justify-center items-center bg-black`}>
-        <Text>Mapa</Text>
-      </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <ArrowBackComponent edit="absolute top-6 left-5 z-10 rounded-full p-1 bg-white" />
+      <MapView
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+        className={`h-[130px] max-h-[18%] border justify-center items-center `}
+      ></MapView>
       <View className={`h-min border-b-[#FFA800] border-b mx-4 py-4 flex-row`}>
-        <Image source={{ uri: picture.large }} className={`h-[120px] w-[120px] rounded-full`} />
+        <Image
+          source={{ uri: picture.large }}
+          className={`h-[120px] w-[120px] rounded-full`}
+        />
         <View className={`flex-1 justify-evenly items-center `}>
-          <Text className={`text-base font-semibold `}>{name.first} {name.last}</Text>
-          <StarRating rating={3} styles={{ borderBottomWidth: 1, borderBottomColor: "#FFA800", paddingBottom: 2 }} />
-          <TouchableOpacity className={`flex-row items-center gap-1 text-base font-medium `}>
+          <Text className={`text-base font-semibold `}>
+            {name.first} {name.last}
+          </Text>
+          <StarRating
+            rating={3}
+            styles={{
+              borderBottomWidth: 1,
+              borderBottomColor: "#FFA800",
+              paddingBottom: 2,
+            }}
+          />
+          <TouchableOpacity
+            className={`flex-row items-center gap-1 text-base font-medium `}
+          >
             <ChatCircleText width={16} height={16} />
             <Text className={`font-medium text-base`}>Contactar</Text>
           </TouchableOpacity>
@@ -54,9 +93,7 @@ const ReqTripScreen = ({ route }: { route: RouteProp<any> }) => {
       <View className={`border-b border-b-[#FFA800] h-min mx-4 py-4 gap-y-1`}>
         <View className={`flex-row items-center gap-x-1`}>
           <Calendar width={20} height={20} />
-          <Text className={`text-base font-normal`}>
-            {day}
-          </Text>
+          <Text className={`text-base font-normal`}>{day}</Text>
         </View>
         <View className={` pl-2 gap-y-[2px] max-w-[70%]`}>
           <View className={`flex-row items-center  ml-[-3px] mb-[-5px] `}>
@@ -66,7 +103,9 @@ const ReqTripScreen = ({ route }: { route: RouteProp<any> }) => {
           <PointedLine />
           <View className={`flex-row items-start ml-[-11px]`}>
             <MapPin width={24} height={24} />
-            <Text className={`ml-1 text-base`}>{`${location.city}, ${location.state}`}</Text>
+            <Text
+              className={`ml-1 text-base`}
+            >{`${location.city}, ${location.state}`}</Text>
           </View>
         </View>
         <View className={`absolute right-0 top-3 gap-y-1`}>
@@ -77,7 +116,9 @@ const ReqTripScreen = ({ route }: { route: RouteProp<any> }) => {
           </View>
         </View>
       </View>
-      <View className={`h-min border-b-[#FFA800] border-b mx-4 py-4 gap-y-[20]`}>
+      <View
+        className={`h-min border-b-[#FFA800] border-b mx-4 py-4 gap-y-[20]`}
+      >
         <View className={`flex-row items-center gap-x-1`}>
           <Car width={20} height={20} />
           <Text className={`text-base font-semibold`}>
@@ -101,23 +142,22 @@ const ReqTripScreen = ({ route }: { route: RouteProp<any> }) => {
         <View className={`flex-row justify-between`}>
           <View className={`flex-row items-center gap-x-1`}>
             <CreditCard width={20} height={20} />
-            <Text className={`text-base font-normal`}>
-              Medio de pago:
-            </Text>
+            <Text className={`text-base font-normal`}>Medio de pago:</Text>
           </View>
           <View className={`flex-row items-center gap-x-1`}>
             <Visa width={35} height={35} />
-            <Text className={`text-base font-normal`}>
-              **** 0253
-            </Text>
+            <Text className={`text-base font-normal`}>**** 0253</Text>
             <ArrowDropDown width={24} height={24} />
           </View>
         </View>
       </View>
-      <ButtonReqTravel requestSent={requestSent} requestAccepted={requestAccepted} setRequestSent={setRequestSent}
+      <ButtonReqTravel
+        requestSent={requestSent}
+        requestAccepted={requestAccepted}
+        setRequestSent={setRequestSent}
       />
     </ScrollView>
-  )
-}
+  );
+};
 
-export default ReqTripScreen
+export default ReqTripScreen;
